91f6489a33d3c38e47fbc3315e2f6c0a5dd06ad9
David Avetian <david@avetian.se>
2025-11-24 18:58:49 +0100

    First working commit. Most likely includes some bugs and errors. Need to do a checkup

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ I did some changes to the data class Artikel since there is a line in the Delphi
 In this Line Netto takes a value from Artikel.inkop where in Delphi it's a string value that can be empty.
 In Kotlin this value is defined as a double, but I am allowing it to be null and checking for it for backwards compatibility.
 This should replicate the Delphi behavior and work with older databases that can possibly input an empty string where one expects a double.
+
+Since the inkop value is defined as a double in the Artikel dataclass, I will not into take account situations where inkop can be an empty string.
+It would be fairly simple to just change the dataclass to a string and use a checker if the string is a double, like Delphis IsFloatString().
+The instructions however say that I should just assume that all numeric values are doubles, so I will only check for null in this case.
+This is intended and will not work if the code gets a '' value in the database.
+
+I am however thinking of updating this once I get the code running by assuming everything is double.
--- WinAssistKodtest.kt
+++ WinAssistKodtest.kt
@@ -9,49 +9,143 @@ fun main() {
 
 class KodTest {
     fun TestaInkopspriserBtnClick(lagerPoster: List<LagerPost>): Pair<Int?,List<String>?> {
-	var ArtMK: String
-	var ArtNr: String
-	var Typ: String
-	var Kod: String
-	var MK: String
-	var RK: String
-	var VG: String
-	var PG: String
-	var Benamning: String
-	var LgrStatus: String
-	var Brutto: Double
-	var Netto: Double?
+	var ArtMK: String = ""
+	var ArtNr: String = ""
+	var Typ: String = ""
+	var Kod: String = ""
+	var MK: String = ""
+	var RK: String = ""
+	var VG: String = ""
+	var PG: String = ""
+	var Benamning: String = ""
+	var LgrStatus: String = ""
+
+	// Im using these two for the cases where I need to use Brutto and Netto as Str since I'm not supposed to use IsFloatString()
+	var BruttoStr = ""
+	var NettoStr = ""
+
+	var Brutto: Double = 0.0
+	var Netto: Double? = 0.0
 	var TaMed: Boolean
 	var Antal = 0
 	var gAktuellInkopsrabatt = 1;
+
+	// This isn't actually used in the code but I'm leaving this here since the original Delphi code has this.
 	var OrderFields = "mk"
-	var LagerRe = mutableListOf<String>();
+	var LagerRE = mutableListOf<String>();
+	LagerRE.clear()
 	for (lagerPost in lagerPoster) {
 	    ArtMK = lagerPost.artikelMk
 	    LgrStatus = lagerPost.lgrStatus
 	    TaMed = false
+
+	    // Can make the following if-statement prettier by using enums or something similar
 	    if (LgrStatus == "lgrJA" || LgrStatus == "lgrHEMTAGEN" || LgrStatus == "lgrUTGAENDE") {
 		var artikel = DM.getArtikel(ArtMK)
+
 		if (artikel != null) {
 		    Antal++
 		    ArtNr = artikel.artikelnr
 		    MK = artikel.mk
 		    Benamning = artikel.benamning1
 		    RK = artikel.rabattKod
-		    VG  = artikel.varuGrupp
-		    PG  = artikel.prodGrupp
+		    VG = artikel.varuGrupp
+		    PG = artikel.prodGrupp
+
 		    if (artikel.fastPris) {
 			Typ = "F"
 			Kod = "IS"
 			Brutto = artikel.pris
 			Netto = artikel.inkop
+
+			// Maybe I should use NettoStr here? But artikel.inkop is by default a double so it makes no sense. I will leave this as null instead.
 			TaMed = (Netto == null)
-			println(TaMed)
+		    }
+
+		    else if (artikel.useNetto) {
+			Typ = "V"
+			Brutto = artikel.vNetto
+			Netto = getInkopsPrisVNetto(Brutto, MK, RK, VG, PG)
+			TaMed = (RK.isEmpty()) || (Netto >= Brutto)
+
+			if (RK.isEmpty()) {
+			    Kod = "RS"
+			}
+
+			else {
+			    Kod = "RV"
+			}
+		    }
+
+		    else {
+			Typ = "B"
+			Brutto = artikel.pris
+			Netto = getInkopsPris(Brutto, MK, RK, PG, VG)
+			TaMed = (RK.isEmpty()) || (Netto >= Brutto)
+
+			if (RK.isEmpty()) {
+			    Kod = "RS"
+			}
+
+			else {
+			    Kod = "RV"
+			}
 		    }
 		}
+
+		else {
+		    ArtNr = lagerPost.artikelnr
+		    MK = lagerPost.mk
+		    Benamning = L_(199, "SAKNAS I ARTIKELREGISTER!!")
+		    Typ = ""
+		    Kod = ""
+		    RK = ""
+
+		    // In the Delphi code this is set to "" but I will assume they're double here and use BruttoStr and NettoStr for string inputs, so these can possibly be removed.
+		    Brutto = 0.0
+		    Netto = 0.0
+		    TaMed = true
+		}
 	    }
+
+	    if (TaMed) {
+		Typ = AddBlanks(Typ, 4, alLEFT)
+		Kod = AddBlanks(Kod, 4, alLEFT)
+		ArtNr = AddBlanks(ArtNr, 18, alLEFT)
+		MK = AddBlanks(MK, 5, alLEFT)
+		RK = AddBlanks(RK, 5, alLEFT)
+		Benamning = AddBlanks(Benamning, 30, alLEFT)
+		NettoStr = AddBlanks(NettoStr, 10, alRIGHT)
+		BruttoStr = AddBlanks(BruttoStr, 10, alRIGHT)
+		LagerRE.add(Typ+Kod+ArtNr+MK+RK+Benamning+NettoStr+BruttoStr)
+	    }
+
 	}
-	return Pair(null, null)
+	OrderFields = ""
+	LagerRE.add("")
+	LagerRE.add(L_(200, "Typer:"))
+	LagerRE.add(L_(201, "B = Inköpspris beräknat på rabattkod mot bruttopris"))
+	LagerRE.add(L_(202, "V = Inköpspris beräknat på rabattkod mot verkstadsnetto"))
+	LagerRE.add(L_(203, "F = Fast inköpspris"))
+
+	LagerRE.add("")
+	LagerRE.add(L_(204, "Koder:"))
+	LagerRE.add(L_(205, "IS = Inköpspris saknas"))
+	LagerRE.add(L_(206, "RS = Rabattkod saknas"))
+	LagerRE.add(L_(207, "RV = Rabattkod saknar värde"))
+	Typ = AddBlanks("Typ", 4, alLEFT)
+	Kod = AddBlanks("Kod", 4, alLEFT)
+	ArtNr = AddBlanks(L_(37, "Artikelnr"), 18, alLEFT)
+	MK = AddBlanks("MK", 5, alLEFT)
+	RK = AddBlanks("RK", 5, alLEFT)
+	Benamning = AddBlanks(L_(98, "Benämning"), 30, alLEFT)
+	NettoStr = AddBlanks(L_(209, "Inköp"), 10, alRIGHT)
+	BruttoStr = AddBlanks(L_(102, "Pris"), 10, alRIGHT)
+	LagerRE.add(0, "--------------------------------------------------------------------------------------\n")
+	LagerRE.add(0, Typ+Kod+ArtNr+MK+RK+Benamning+NettoStr+BruttoStr)
+	println(Antal)
+	println(LagerRE)
+	return Pair(Antal, LagerRE)
     }
 
     fun getInkopsPris(brutto: Double, mk: String, rk: String, vg: String, pg: String): Double {
